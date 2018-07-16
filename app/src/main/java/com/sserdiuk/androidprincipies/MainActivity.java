18344968b15b7f8247d99c9c91fae167d6814986
Serdiuk Serhii <serduksergey90@gmail.com>
2018-07-16 15:49:05 +0300

    add repo with fragments services and boradcastreceivers

---
+++ app/src/main/java/com/sserdiuk/androidprincipies/MainActivity.java
@@ -0,0 +1,26 @@
+package com.sserdiuk.androidprincipies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onFragmentClick(View view) {
+    }
+
+    public void onServiceClick(View view) {
+    }
+
+    public void onBroadcastClick(View view) {
+    }
+
+    public void onStopServiceClick(View view) {
+    }
+}

9bb073bd92720182f75e80820e55c315a7f68d79
Serdiuk Serhii <serduksergey90@gmail.com>
2018-07-16 19:00:43 +0300

    replace with switchcase

--- app/src/main/java/com/sserdiuk/androidprincipies/ForegroundService.java
+++ app/src/main/java/com/sserdiuk/androidprincipies/ForegroundService.java
@@ -34,42 +34,43 @@ public class ForegroundService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent.getAction().equals(Constans.ACTION.STARTFOREGROUND_ACTION)) {
-            Log.i(TAG, "Received Start Foreground Intent ");
-            Intent notificationIntent = new Intent(this, MainActivity.class);
-            notificationIntent.setAction(Constans.ACTION.MAIN_ACTION);
-            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                    | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
-                    notificationIntent, 0);
+        switch (intent.getAction()) {
+            case Constans.ACTION.STARTFOREGROUND_ACTION:
+                Log.i(TAG, "Received Start Foreground Intent ");
+                Intent notificationIntent = new Intent(this, MainActivity.class);
+                notificationIntent.setAction(Constans.ACTION.MAIN_ACTION);
+                notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                        notificationIntent, 0);
 
-            Intent previousIntent = new Intent(this, ForegroundService.class);
-            previousIntent.setAction(Constans.ACTION.PREV_ACTION);
-            PendingIntent ppreviousIntent = PendingIntent.getService(this, 0,
-                    previousIntent, 0);
+                Intent previousIntent = new Intent(this, ForegroundService.class);
+                previousIntent.setAction(Constans.ACTION.PREV_ACTION);
+                PendingIntent ppreviousIntent = PendingIntent.getService(this, 0,
+                        previousIntent, 0);
 
-            Intent playIntent = new Intent(this, ForegroundService.class);
-            playIntent.setAction(Constans.ACTION.PLAY_ACTION);
-            PendingIntent pplayIntent = PendingIntent.getService(this, 0,
-                    playIntent, 0);
+                Intent playIntent = new Intent(this, ForegroundService.class);
+                playIntent.setAction(Constans.ACTION.PLAY_ACTION);
+                PendingIntent pplayIntent = PendingIntent.getService(this, 0,
+                        playIntent, 0);
 
-            Intent nextIntent = new Intent(this, ForegroundService.class);
-            nextIntent.setAction(Constans.ACTION.NEXT_ACTION);
-            PendingIntent pnextIntent = PendingIntent.getService(this, 0,
-                    nextIntent, 0);
+                Intent nextIntent = new Intent(this, ForegroundService.class);
+                nextIntent.setAction(Constans.ACTION.NEXT_ACTION);
+                PendingIntent pnextIntent = PendingIntent.getService(this, 0,
+                        nextIntent, 0);
 
-            Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground);
+                Bitmap icon = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher_foreground);
 
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            String channelId = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ? createNotificationChannel(notificationManager) : "";
-            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId);
-            Notification notification = notificationBuilder.setOngoing(true)
-                    .setSmallIcon(R.mipmap.ic_launcher)
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                String channelId = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ? createNotificationChannel(notificationManager) : "";
+                NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId);
+                Notification notification = notificationBuilder.setOngoing(true)
+                        .setSmallIcon(R.mipmap.ic_launcher)
 //                    .setPriority(PRIORITY_MIN)
 
-                    /**
-                     * FOR UI PART for clickable buttons and etc
-                     * */
+                        /*
+                         * FOR UI PART for clickable buttons and etc
+                         * */
 //                    .setContentTitle("Truiton Music Player")
 //                    .setTicker("Truiton Music Player")
 //                    .setContentText("My Music")
@@ -83,22 +84,27 @@ public class ForegroundService extends Service {
 //                    .addAction(android.R.drawable.ic_media_next, "Next",
 //                            pnextIntent)
 
-                    .setCategory(NotificationCompat.CATEGORY_SERVICE)
-                    .build();
+                        .setCategory(NotificationCompat.CATEGORY_SERVICE)
+                        .build();
 
 
-            startForeground(Constans.NOTIFICATION_ID.FOREGROUND_SERVICE, notification);
+                startForeground(Constans.NOTIFICATION_ID.FOREGROUND_SERVICE, notification);
 
-        } else if (intent.getAction().equals(Constans.ACTION.PREV_ACTION)) {
-            Log.i(TAG, "Clicked Previous");
-        } else if (intent.getAction().equals(Constans.ACTION.PLAY_ACTION)) {
-            Log.i(TAG, "Clicked Play");
-        } else if (intent.getAction().equals(Constans.ACTION.NEXT_ACTION)) {
-            Log.i(TAG, "Clicked Next");
-        } else if (intent.getAction().equals(Constans.ACTION.STOPFOREGROUND_ACTION)) {
-            Log.i(TAG, "STOP FOREGROUND SERVICE");
-            stopForeground(true);
-            stopSelf();
+                break;
+            case Constans.ACTION.PREV_ACTION:
+                Log.i(TAG, "Clicked Previous");
+                break;
+            case Constans.ACTION.PLAY_ACTION:
+                Log.i(TAG, "Clicked Play");
+                break;
+            case Constans.ACTION.NEXT_ACTION:
+                Log.i(TAG, "Clicked Next");
+                break;
+            case Constans.ACTION.STOPFOREGROUND_ACTION:
+                Log.i(TAG, "STOP FOREGROUND SERVICE");
+                stopForeground(true);
+                stopSelf();
+                break;
         }
 
         return START_STICKY;

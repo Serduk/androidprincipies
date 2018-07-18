57d6caefc2d6c7a04bfe14f11822ee8b57eaf604
Serdiuk Serhii <serduksergey90@gmail.com>
2018-07-18 19:36:26 +0300

    cleanup main activity. create new activity for fragments

---
+++ app/src/main/java/com/sserdiuk/androidprincipies/ForFragmentsTestingActivity.java
@@ -0,0 +1,13 @@
+package com.sserdiuk.androidprincipies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForFragmentsTestingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_for_fragments_testing);
+    }
+}
--- app/src/main/java/com/sserdiuk/androidprincipies/MainActivity.java
+++ app/src/main/java/com/sserdiuk/androidprincipies/MainActivity.java
@@ -1,41 +1,21 @@
 package com.sserdiuk.androidprincipies;
 
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements TestFragment.OnFragmentInteractionListener {
-    FragmentTransaction ft;
-
-    TestFragment testFragment;
-    TestFragmentTwo testFragmentTwo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        testFragment = getFragmentManager().findFragmentById(R.id.fragment_container);
-
-        ft  = getSupportFragmentManager().beginTransaction();
-        ft.add(R.id.fragment_placeholder, testFragment);
-        ft.commit();
-
     }
 
     public void onFragmentClick(View view) {
-        testFragment = new TestFragment();
-        testFragmentTwo = new TestFragmentTwo();
-
-        ft.addToBackStack(null);
-//        ft.remove(fragment);
-        ft.replace(R.id.fragment_placeholder, testFragmentTwo);
-//        ft.add(fragment, "TAG1");
-        ft.commit();
+        startActivity(new Intent(this, ForFragmentsTestingActivity.class));
     }
 
     public void onServiceClick(View view) {
